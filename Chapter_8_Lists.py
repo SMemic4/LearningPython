aea38978c1dbe3770cf482e449fe30895a78181c
SMemic4 <129231670+SMemic4@users.noreply.github.com>
2023-06-10 20:31:02 -0400

    Update Chapter_8_Lists.py

--- Chapter_8_Lists.py
+++ Chapter_8_Lists.py
@@ -341,7 +341,13 @@ print(letters) # ['b', 'c']
 
 t1 = [1, 2]
 t2 = t1.append(3)
+print(t1) # [1, 2, 3]
+print(t2) # None
 
+t1 = [1, 2]
+t3 = t1 + [3]
+print(t3) # [1, 2, 3]
+t1 is t3 # false
 
 
 
